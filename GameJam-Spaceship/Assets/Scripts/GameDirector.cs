90edd8711f8dc691a7af4c5e1eb273d4610482ed
Chrriiisss <christopher.smith6@hotmail.co.uk>
2020-02-01 11:39:38 +0000

    Update GameDirector.cs

--- GameJam-Spaceship/Assets/Scripts/GameDirector.cs
+++ GameJam-Spaceship/Assets/Scripts/GameDirector.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameDirector : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class GameDirector : MonoBehaviour {
+
+    [SerializeField] private readonly float baseRNG = 0.1f;
+    private float modifiers = 0f;
+    [SerializeField] private bool shieldsActive;
+
+    public void ModifyRNG(float amount) {
+        modifiers += amount;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private float GetRNG() {
+        return baseRNG + modifiers;
     }
+
+    private void Start() {
+        shieldsActive = true;
+    }
+
+    private void Update() {
+        // Do Random Damage checks
+    }
+
 }
