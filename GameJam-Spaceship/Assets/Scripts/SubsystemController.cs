a34fbb5b5d77a89d6c86b7babdeab0b5df32be07
Chrriiisss <christopher.smith6@hotmail.co.uk>
2020-02-01 15:44:49 +0000

    Rename to SubsystemController

--- GameJam-Spaceship/Assets/Scripts/SubsystemController.cs
+++ GameJam-Spaceship/Assets/Scripts/SubsystemController.cs
@@ -2,23 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class SubsystemController : MonoBehaviour {
 
-    private int shipHealth; // Health of the ship, reaching 0 means game over.
     private readonly List<ISubsystem> subsystems = new List<ISubsystem>();
     // Start is called before the first frame update
     void Start() {
-        this.shipHealth = 100;
         this.subsystems.Add(GameObject.Find("WirelessCharging").GetComponent<WirelessCharging>());
         this.subsystems.Add(GameObject.Find("ShieldGenerator").GetComponent<ShieldGenerator>());
         this.subsystems.Add(GameObject.Find("OxygenGenerator").GetComponent<OxygenGenerator>());
         this.subsystems.Add(GameObject.Find("Navigation").GetComponent<Navigation>());
         this.subsystems.Add(GameObject.Find("Engines").GetComponent<Engines>());
         // this.subsystems.Add(GameObject.Find("ShipComputer").GetComponent<WirelessCharging>()); !!Potential extension!!
-    }
-
-    int GetShipHealth() {
-        return this.shipHealth;
     }
 
     public List<ISubsystem> GetSubsystems() {
