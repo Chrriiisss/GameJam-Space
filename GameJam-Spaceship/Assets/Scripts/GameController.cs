20451d145473bc598d94901683595239dbd378d6
Chrriiisss <christopher.smith6@hotmail.co.uk>
2020-02-01 14:25:04 +0000

    Update to Game Mechanics

--- GameJam-Spaceship/Assets/Scripts/GameController.cs
+++ GameJam-Spaceship/Assets/Scripts/GameController.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
     private int shipHealth; // Health of the ship, reaching 0 means game over.
-    private List<ISubsystem> subsystems = new List<ISubsystem>();
+    private readonly List<ISubsystem> subsystems = new List<ISubsystem>();
     // Start is called before the first frame update
     void Start() {
         this.shipHealth = 100;
-        subsystems.Add(GameObject.Find("WirelessCharging").GetComponent<WirelessCharging>());
-        subsystems.Add(GameObject.Find("ShieldGenerator").GetComponent<ShieldGenerator>());
-        subsystems.Add(GameObject.Find("OxygenGenerator").GetComponent<OxygenGenerator>());
-        subsystems.Add(GameObject.Find("Navigation").GetComponent<Navigation>());
-        subsystems.Add(GameObject.Find("Engines").GetComponent<Engines>());
-        //  subsystems.Add(GameObject.Find("ShipComputer").GetComponent<WirelessCharging>()); !!Potential extension!!
+        this.subsystems.Add(GameObject.Find("WirelessCharging").GetComponent<WirelessCharging>());
+        this.subsystems.Add(GameObject.Find("ShieldGenerator").GetComponent<ShieldGenerator>());
+        this.subsystems.Add(GameObject.Find("OxygenGenerator").GetComponent<OxygenGenerator>());
+        this.subsystems.Add(GameObject.Find("Navigation").GetComponent<Navigation>());
+        this.subsystems.Add(GameObject.Find("Engines").GetComponent<Engines>());
+        // this.subsystems.Add(GameObject.Find("ShipComputer").GetComponent<WirelessCharging>()); !!Potential extension!!
     }
 
     int GetShipHealth() {
