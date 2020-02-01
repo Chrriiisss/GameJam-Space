9c700840d9c8515c3f65427d72c1286c2947220a
Mikolaj <mfb2g15@soton.ac.uk>
2020-02-01 15:45:28 +0000

    Making repair guage

---
+++ GameJam-Spaceship/Assets/scripts/RepairGaugeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairGaugeController : MonoBehaviour
+{
+    
+    public SpriteRenderer ren;
+
+    public List<Sprite> sprites;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ren.sprite = sprites[Random.Range(0,3)];
+        ren.transform.Rotate(0f, 0f, Random.Range(-20f, -140f));
+    }
+
+}
