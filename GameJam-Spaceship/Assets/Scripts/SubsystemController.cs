8ac996aa105752672479861e87c3117aa0b7215d
Mikolaj <mfb2g15@soton.ac.uk>
2020-02-01 21:31:57 +0000

    no idea what i added lol

--- GameJam-Spaceship/Assets/Scripts/SubsystemController.cs
+++ GameJam-Spaceship/Assets/Scripts/SubsystemController.cs
@@ -7,11 +7,11 @@ public class SubsystemController : MonoBehaviour {
     private readonly List<ISubsystem> subsystems = new List<ISubsystem>();
     // Start is called before the first frame update
     void Start() {
-        this.subsystems.Add(GameObject.Find("WirelessCharging").GetComponent<WirelessCharging>());
+      /*  this.subsystems.Add(GameObject.Find("WirelessCharging").GetComponent<WirelessCharging>());
         this.subsystems.Add(GameObject.Find("ShieldGenerator").GetComponent<ShieldGenerator>());
         this.subsystems.Add(GameObject.Find("OxygenGenerator").GetComponent<OxygenGenerator>());
         this.subsystems.Add(GameObject.Find("Navigation").GetComponent<Navigation>());
-        this.subsystems.Add(GameObject.Find("Engines").GetComponent<Engines>());
+        this.subsystems.Add(GameObject.Find("Engines").GetComponent<Engines>());*/
         // this.subsystems.Add(GameObject.Find("ShipComputer").GetComponent<WirelessCharging>()); !!Potential extension!!
     }
 
--- GameJam-Spaceship/Assets/scripts/RepairController.cs
+++ GameJam-Spaceship/Assets/scripts/RepairController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RepairController : MonoBehaviour
 {
+
+    public float detectDistance = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +13,17 @@ public class RepairController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        RaycastHit hit;
+
+         if(Physics.Raycast(transform.position, transform.forward, out hit, detectDistance)     &&
+                     hit.collider.gameObject.CompareTag("Repairable")) {
+ 
+             
+
+             
+
+         }
     }
 }
