3c9ffcfcc5f6875570ecc0858c1e4fe73b5888de
Chrriiisss <christopher.smith6@hotmail.co.uk>
2020-02-01 17:20:18 +0000

    Add MainMenu

---
+++ GameJam-Spaceship/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Space)) {
+            SceneManager.LoadScene("TheGame");
+        }        
+    }
+}
