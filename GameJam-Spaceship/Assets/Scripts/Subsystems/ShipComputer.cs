555c8904c348a094f866e5b74e9f3063deb42b46
Chrriiisss <christopher.smith6@hotmail.co.uk>
2020-02-01 11:21:55 +0000

    Fixed new Subsystems

--- GameJam-Spaceship/Assets/Scripts/Subsystems/ShipComputer.cs
+++ GameJam-Spaceship/Assets/Scripts/Subsystems/ShipComputer.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipComputer : MonoBehaviour
-{
+public class ShipComputer : MonoBehaviour, ISubsystem {
+
+    private int componentHealth;
+    private readonly int maxHealth = 100;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        this.componentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public int GetHealth() {
+        return this.componentHealth;
+    }
+
+    public void TakeDamage(int damageAmount) {
+        this.componentHealth = Mathf.Max(0, this.componentHealth - damageAmount);
+    }
+
+    public void Repair() {
+        this.componentHealth = maxHealth;
+    }
+
+    private void ActivateEffect() {
+
     }
 }
