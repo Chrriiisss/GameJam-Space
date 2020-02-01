e9909dafb01a42137dff4bacb1b1ef29aaa1e0f8
Chrriiisss <christopher.smith6@hotmail.co.uk>
2020-02-01 15:15:16 +0000

    Asteroid mechanics

--- GameJam-Spaceship/Assets/Scripts/GameDirector.cs
+++ GameJam-Spaceship/Assets/Scripts/GameDirector.cs
@@ -6,27 +6,37 @@ public class GameDirector : MonoBehaviour {
 
     [SerializeField] private readonly float baseRNG = 0.1f;
     [SerializeField] private readonly float baseSpeed = 1f;
-    private float modifiers = 0f;
+    [SerializeField] private float calculatedAsteroidChance;
     [SerializeField] private bool shieldsActive;
     [SerializeField] private float distanceToEnd;
     [SerializeField] private float speed;
+
+    private readonly float minTimeBetweenAsteroids = 5f;
+    private float timeSinceLastAsteroid = 0f;
+    private float randomModifiers = 0f;
 
     private void Start() {
         shieldsActive = true;
         distanceToEnd = 1000f;
         speed = baseSpeed;
     }
-
+    
     private void Update() {
-        // Do Random Damage checks
+        if (timeSinceLastAsteroid > minTimeBetweenAsteroids) {
+            calculatedAsteroidChance = baseRNG + randomModifiers;
+            if (Random.Range(0f, 1f) < calculatedAsteroidChance) {
+                timeSinceLastAsteroid = 0f;
+                DamageRandomSubsystem();
+            }
+        }
     }
 
     public void ModifyRNG(float amount) {
-        modifiers += amount;
+        randomModifiers += amount;
     }
 
     private float GetRNG() {
-        return baseRNG + modifiers;
+        return baseRNG + randomModifiers;
     }
 
     public void SetShields(bool state) {
@@ -42,4 +52,7 @@ public class GameDirector : MonoBehaviour {
         }
     }
 
+    private void DamageRandomSubsystem() {
+
+    }
 }
