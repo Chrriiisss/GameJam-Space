ca2e9d4aabd19fbe3eef8697ace155c826279343
Cam <cam@camshepherd.com>
2020-02-02 09:50:37 +0000

    Say something

--- GameJam-Spaceship/Assets/TestWindowsVoice.cs
+++ GameJam-Spaceship/Assets/TestWindowsVoice.cs
@@ -7,7 +7,7 @@ public class TestWindowsVoice : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        WindowsVoice.speak("The generator is running at 50%");
     }
 
     // Update is called once per frame
