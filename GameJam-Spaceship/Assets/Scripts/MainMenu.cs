df4c03edd3698511c5ed998f4f4cc03ade59e70e
Cam <cam@camshepherd.com>
2020-02-02 10:50:24 +0000

    Improve menus

--- GameJam-Spaceship/Assets/Scripts/MainMenu.cs
+++ GameJam-Spaceship/Assets/Scripts/MainMenu.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
+
+    private AudioSource source;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
 
     void Update() {
         if (Input.GetKey(KeyCode.Space)) {
-            if (SceneManager.GetActiveScene().name == "GameWin") {
+            if (SceneManager.GetActiveScene().name == "GameWin")
+            {
+                source.Play();
+                float delayTime = 1.0f;
+                float timer = 0.0f;
+                while (timer < delayTime)
+                {
+                    timer += Time.deltaTime;
+                }
                 SceneManager.LoadScene("MainMenu");
             }
-            else {
+            else
+            {
+                source.loop = false;
+                source.Play();
+                float delayTime = 1.0f;
+                float timer = 0.0f;
+                while (timer < delayTime)
+                {
+                    timer += Time.deltaTime;
+                }
                 SceneManager.LoadScene("TheGame");
             }            
         }        
