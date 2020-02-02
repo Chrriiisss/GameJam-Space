0496b6d9279ec0b85c02ae2737a275aea281279a
Mikolaj <mfb2g15@soton.ac.uk>
2020-02-02 07:44:06 +0000

    Please don't fuck this up

---
+++ GameJam-Spaceship/Assets/scripts/HealthBarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarController : MonoBehaviour
+{
+
+    public RectTransform bar;
+    public float health = 1f;
+
+
+
+    void FixedUpdate() {
+        bar.sizeDelta = new Vector2(health, bar.sizeDelta.y);
+        bar.transform.localPosition = new Vector2((health - 300)/2f, bar.transform.localPosition.y);
+
+        
+    }
+
+    
+
+}
--- GameJam-Spaceship/Assets/scripts/RepairController.cs
+++ GameJam-Spaceship/Assets/scripts/RepairController.cs
@@ -5,25 +5,45 @@ using UnityEngine;
 public class RepairController : MonoBehaviour
 {
 
-    public float detectDistance = 10f;
+    private Camera cam;
+
+    public Canvas canvas;
+
+    public GameObject healthBarPre;
+
+    public bool showing = false;
+    
+    
+    public float detectDistance = 100f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        
+
+        Vector3 CameraCenter = cam.ScreenToWorldPoint(new Vector3(Screen.width/2f, Screen.height/2f, cam.nearClipPlane));
+        
         RaycastHit hit;
 
-         if(Physics.Raycast(transform.position, transform.forward, out hit, detectDistance)     &&
+         if(Physics.Raycast(CameraCenter, cam.transform.forward, out hit, detectDistance)     &&
                      hit.collider.gameObject.CompareTag("Repairable")) {
  
-             
+                    if(!showing) {
+                        GameObject healthBar = Instantiate(healthBarPre, new Vector3(0,0,0), Quaternion.identity, canvas.transform);
+                        healthBar.transform.position = new Vector3(0,0,0);
+                        
+                    }
+                    showing = true;
+                    
+            }
 
              
 
-         }
+         
     }
 }
